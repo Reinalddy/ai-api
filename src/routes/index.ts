ab4799d7ee52b4161696453b9f1b738988764abe
Reinalddy <reinalddyaldy183@gmail.com>
2024-11-08 20:35:26 +0700

    update prisma

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import * as controller from "../controllers/index";
 
-export const index = Router();
+export const router = Router();
 
-index.get("/", controller.index);
+router.get("/", controller.index);
+// router.post("/login" );
